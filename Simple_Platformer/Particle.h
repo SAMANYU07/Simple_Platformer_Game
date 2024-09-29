0d6aad30574baa50bc4f17884066d481b2fedb86
SAMANYU07 <samanyu1003@gmail.com>
2024-09-29 19:42:14 +0530

    Optimized Code

--- Simple_Platformer/Particle.h
+++ Simple_Platformer/Particle.h
@@ -19,3 +19,27 @@ public:
 		this->body.setScale(2, 1);
 	}
 };
+
+Particle* ParticleArray[10];
+
+float randomSpeed()
+{
+	std::random_device rd;
+	std::mt19937 generator(rd());
+	std::uniform_real_distribution<float> distribution(-0.2, -0.1);
+	float random_number = distribution(generator);
+	return random_number;
+}
+
+void particleInit(Particle& p)
+{
+	p.speed = randomSpeed();
+	p.body.setPosition(rand() % 1920, rand() % 200);
+}
+
+void particleGenerator(Particle& p)
+{
+	if (p.body.getPosition().x < -100)
+		p.body.setPosition(1920 + rand() % 200, rand() % 200);
+	p.body.move(p.speed, 0);
+}
